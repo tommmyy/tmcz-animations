83b4423f6896ec876aa4ac0dc6407e9259d9f903
Tomáš Konrády <tomaskonrady@gmail.com>
2018-12-11 10:39:40 +0100

    styled solution

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Box, Heading, Text } from 'rebass';
+import React, { Component } from 'react';
+import { Box, Heading, Button } from 'rebass';
+import styled from 'styled-components';
 
-const Content = ({ ...rest }) => (
-	<Box>
-		<Heading>Hello</Heading>
-		<Text>World</Text>
-	</Box>
-);
+const Animation = styled.div`
+	opacity: ${(ps) => (ps.checked ? 1 : 0)};
+	transition: opacity 0.3s ease-in;
+
+	background-color: lime;
+	width: 50px;
+	height: 50px;
+`;
+
+class Content extends Component {
+	state = { checked: false };
+
+	handleClick = () => this.setState(({ checked }) => ({ checked: !checked }));
+
+	render() {
+		const { checked } = this.state;
+		return (
+			<Box p={8}>
+				<Heading>Basic animation</Heading>
+				<Box my={4}>
+					<Animation checked={checked} />
+				</Box>
+				<Button onClick={this.handleClick}>Toggle</Button>
+			</Box>
+		);
+	}
+}
 
 Content.defaultProps = {};
 
