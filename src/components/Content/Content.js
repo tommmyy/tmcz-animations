275113d6a55bad8799fa6f08e98d2ee3955b6a1f
Tomáš Konrády <tomaskonrady@gmail.com>
2018-12-11 12:26:34 +0100

    intermediate

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import { Flex, Box, Heading } from 'rebass';
-import cx from 'classnames';
-import './ProgressBar.css';
+import styled, { css, keyframes } from 'styled-components';
 
-const ProgressBar = ({ value }) => (
-	<div className={cx('ProgressBar')}>
-		<div className={cx('ProgressBar-bar')} style={{ transform: `scaleX(${value / 100})` }} />
-	</div>
+const ProgressBarWrapper = styled.div`
+	background-color: lightgray;
+	overflow: hidden;
+`;
+const inter = keyframes`
+  from {
+    transform: translateX(0) scaleX(0.2);
+  }
+
+  to {
+	    transform: translateX(100%) scaleX(0.2);
+	}
+`;
+
+const ProgressBarBar = styled.div`
+	background-color: lime;
+	height: 4px;
+	transform-origin: left;
+	transform: scaleX(${(ps) => ps.value / 100});
+	transition: transform 0.3s ease;
+	${(ps) =>
+		ps.intermediate &&
+		css`
+			transition-property: none;
+			animation: ${inter} 1s linear infinite;
+		`};
+`;
+
+const ProgressBar = ({ value, intermediate }) => (
+	<ProgressBarWrapper>
+		{intermediate && <ProgressBarBar intermediate />}
+		{!intermediate && <ProgressBarBar value={value} />}
+	</ProgressBarWrapper>
 );
 
 class Content extends Component {
@@ -38,6 +66,9 @@ class Content extends Component {
 				<Heading>ProgressBar demo</Heading>
 				<Box my={4} width={1 / 2}>
 					<ProgressBar value={this.state.completed} />
+				</Box>
+				<Box my={4} width={1 / 2}>
+					<ProgressBar intermediate />
 				</Box>
 			</Flex>
 		);
